b5b7dba307cfba51f36784d0f0b5ecebcb125288
nopoe <nopoe.dev@gmail.com>
2013-10-24 20:36:52 -0700

    removed console spam

--- src/tex_font_metrics.c
+++ src/tex_font_metrics.c
@@ -115,7 +115,7 @@ tex_font_metrics_t* load_tex_font_metrics( const char *filename )
 	goto bail;
     }
 
-	print_debug(DEBUG_OTHER, "swap bytes: %d", swap_bytes);
+	//print_debug(DEBUG_OTHER, "swap bytes: %d", swap_bytes);
 
     /* Read in texture_width, texture_height, max_ascent, max_descent */
     READ_BYTES( tfm_file, &texture_width, sizeof(int), swap_bytes );
@@ -172,8 +172,8 @@ tex_font_metrics_t* load_tex_font_metrics( const char *filename )
 
 	tfm->char_data[ch_dims.ch] = cd;
 
-	print_debug(DEBUG_OTHER, "character: %c w: %d h: %d x_offset: %d y_offset: %d kern_width: %d y_pixel: %d x_pixel: %d", ch_dims.ch, ch_dims.w, ch_dims.h, ch_dims.x_offset, ch_dims.y_offset, ch_dims.kern_width,
-		ch_dims.y_pixel, ch_dims.x_pixel);
+	//print_debug(DEBUG_OTHER, "character: %c w: %d h: %d x_offset: %d y_offset: %d kern_width: %d y_pixel: %d x_pixel: %d", ch_dims.ch, ch_dims.w, ch_dims.h, ch_dims.x_offset, ch_dims.y_offset, ch_dims.kern_width,
+	//	ch_dims.y_pixel, ch_dims.x_pixel);
     }
 
 
