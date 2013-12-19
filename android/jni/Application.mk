a1bc547c48566c5fc0c461e2362516b8e1a6fbdc
lenniea <lennie_araki@hotmail.com>
2013-12-18 20:22:48 -0800

    Add trebuchet_red.png font for tutorial Attempt to align with GameCircleClient

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -3,4 +3,5 @@
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
 # APP_STL := stlport_static 
 
-APP_ABI := armeabi armeabi-v7a
+#APP_ABI := armeabi armeabi-v7a
+APP_ABI := x86
