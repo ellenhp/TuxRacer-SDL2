4f1c46b14e24a7a2b592b1cc6bd49555a4185c2a
lenniea <lennie_araki@hotmail.com>
2017-10-13 22:34:31 -0700

    Move files to com.moonlite.tuxracer

---
+++ app/src/full/java/com/moonlite/tuxracer/GameActivity.java
@@ -0,0 +1,36 @@
+package com.moonlite.tuxracer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.moonlite.tuxracer.ScoreActivity;
+
+/**
+ * GameActivity implements full version on top of ScoreActivity
+ */
+public class GameActivity extends ScoreActivity {
+	private static final String TAG = "Tuxracer";
+	
+	private static GameActivity mSingleton;
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mSingleton=this;
+		nativeCoursePrice(null);
+	}
+
+	protected void onDestroy() {
+		super.onDestroy();
+		mSingleton=null;
+	}	
+
+	public static native void nativeCoursePrice(String price);
+}
